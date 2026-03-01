254133110a8c4d9c108ab1d4f4d0bd2aa2a15504
Midanka Lahon <midankalahon@gmail.com>
2026-03-01 19:46:59 +0530

    Shared prisma instance code fix

--- src/prisma/client.py
+++ src/prisma/client.py
@@ -1,4 +1,14 @@
 from prisma import Prisma
+import contextvars
+
+_db_var: contextvars.ContextVar = contextvars.ContextVar('db')
+
+def get_db() -> Prisma:
+    try:
+        return _db_var.get()
+    except LookupError:
+        db = Prisma()
+        _db_var.set(db)
+        return db
 
 db = Prisma()
-
